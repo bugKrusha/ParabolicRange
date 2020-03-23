8afc86bd6061eaa963813dd80cef4829aa2934d6
Jon-Tait Beason <jazbo1876@gmail.com>
2020-03-22 17:58:39 -0700

    Initial Commit

---
+++ ParabolicRange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParabolicRange
+//
+//  Created by Jon-Tait Beason on 3/22/20.
+//  Copyright © 2020 Jon-Tait Beason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
